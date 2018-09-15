e1657956d039c555a13a537586474bd01af14b9b
xinghu213839 <xinghu213839@sohu-inc.com>
2018-09-15 11:47:54 +0800

    布局样式修改

--- app/src/main/java/com/robot/recycle/view/RobotRecycleView.java
+++ app/src/main/java/com/robot/recycle/view/RobotRecycleView.java
@@ -1,17 +1,27 @@
 package com.robot.recycle.view;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
+
+import com.robot.recycle.R;
+import com.robot.recycle.RobotUtils;
 
 /**
  * @author xing.hu
  * @since 2018/9/13, 下午7:43
  */
-public  class RobotRecycleView extends FrameLayout {
-   private RelativeLayout mHeaderView;
-   private Context mCtx;
+public class RobotRecycleView extends FrameLayout {
+    private final static int HEADER_CONTAINER_HEIGHT = 50;
+
+
+    private RelativeLayout mHeaderContainer;
+    private Context mCtx;
 
 
     public RobotRecycleView(Context context) {
@@ -19,31 +29,63 @@ public  class RobotRecycleView extends FrameLayout {
         init(context);
     }
 
+    public RobotRecycleView( Context context,  AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+
+    }
+
+    public RobotRecycleView( Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+
+    }
+
+    public RobotRecycleView( Context context,  AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+
+    }
+
     @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return super.onInterceptTouchEvent(ev);
+    }
+
+
+    
+
+
+    /* @Override
     protected void onLayout(boolean changed, int left, int top, int right,
                             int bottom) {
 
 
+    }*/
 
-
-
-    }
-
-    private void init(Context ctx){
+    private void init(Context ctx) {
         mCtx = ctx;
         initView();
     }
 
-    private void initView(){
+    private void initView() {
         createHeaderView();
     }
 
 
-    private void createHeaderView(){
-        mHeaderView = new RelativeLayout(mCtx);
-        addView(mHeaderView);
+    private void createHeaderView() {
+        mHeaderContainer = new RelativeLayout(mCtx);
+        mHeaderContainer.setBackgroundResource(R.color.red);
+        int headerHeight = RobotUtils.dip2px(mCtx, HEADER_CONTAINER_HEIGHT);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, headerHeight);
+        params.gravity = Gravity.TOP;
+        addView(mHeaderContainer, params);
     }
 
 
+    public void setHeaderView(View headerView) {
+
+
+    }
 
 }
