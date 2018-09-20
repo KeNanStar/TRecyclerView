81a59453fb2df7a57dcf75247836de5f266a44e1
xinghu213839 <xinghu213839@sohu-inc.com>
2018-09-20 14:49:36 +0800

    TRecyclerView-添加注释

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# TRecycleView
-实现RecycleView的下拉更新，上滑加载的功能。
+# TRecyclerView
+实现RecyclerView的下拉更新，上滑加载的功能。
--- app/src/main/java/com/robot/recycler/view/TRecyclerView.java
+++ app/src/main/java/com/robot/recycler/view/TRecyclerView.java
@@ -23,35 +23,69 @@ import com.robot.recycler.listener.IPushRefresh;
 /**
  * @author xing.hu
  * @since 2018/9/13, 下午7:43
+ * TRecyclerView
+ *
+ * pull down to refresh data, slide up to load data and the position of loading animation fixed
+ *
+ * the composition of TRecyclerView:
+ *
+ * 1)  TRecyclerView is a viewGroup-frameLayout, include  the header view and  recyclerView;
+ *
+ * 2)  The function of header view is showing loading animation and tips animation;
+ *
+ * 3)  The recycleView has a RecyclerView.Adapter: TRecyclerAdapter, the types of View in TRecyclerAdapter is
+ *
+ *   the footer view and normal view. RecyclerView loads normal view by the RecyclerView.Adapter which needs
+ *
+ *   to be implemented.
+ *
+ *   
+ *
+ *  the implementation of TRecyclerView:
+ *
+ *  1) When the TRecycleView is pulled over a certain distance, after releasing the hand, start to refresh the data.
+ *
+ *  2) When the RecycleView slides up to the last element(Footer View) is visible, start to load data.
+ *
+ *
  */
 public class TRecyclerView extends FrameLayout{
     private int mTouchSlop;
 
-    // 托盘是否被拖动
+    // whether the TRecyclerView be dragged
     private boolean mIsDrag = false;
 
     private HeaderHolder mHeaderHolder;
 
 
-    private float mInitX = -1f;
+    //private float mInitX = -1f;
+
+
     private float mInitY = -1f;
 
     private RecyclerView mRecyclerView;
 
-    //private boolean mIntercept = false;
-
+    //the header view
     private RelativeLayout mHeaderContainer;
 
+    //TRecyclerAdapter include the footer view and normal view
     private TRecyclerAdapter mTAdapter;
 
     private LinearLayoutManager linearLayoutManager;
 
+    //pull-down refresh data callback
     private IPullRefresh mPullRefresh;
+
+    //slide-up loading data callback
     private IPushRefresh mPushRefresh;
 
+    //the height of header view
     private int mHeaderHeight = -1;
+    //the height of tip view
     private int mTipHeight = -1;
+
     private boolean mRefresh = false;
+
     private boolean mLoadMore = false;
 
 
@@ -84,7 +118,7 @@ public class TRecyclerView extends FrameLayout{
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
         int action = ev.getAction();
-
+         //if the recycleView can scroll, then the TRecyclerView doesn't intercept the event.
         if(isUnIntercept()){
             return  false;
         }
@@ -95,6 +129,7 @@ public class TRecyclerView extends FrameLayout{
                 break;
             case MotionEvent.ACTION_MOVE:
                 float y = ev.getY();
+                //if the distance of moving is over the touchSlop, then The TRecyclerView is dragged.
                 if(y-mInitY >= mTouchSlop && !mIsDrag){
                     mIsDrag = true;
                 }
@@ -142,7 +177,8 @@ public class TRecyclerView extends FrameLayout{
             case MotionEvent.ACTION_CANCEL:
                 dist = (event.getY() - mInitY) * TRecycleViewConst.PULL_DRAG_RATE;
                 if(mIsDrag){
-                    //开始回弹的动画
+                    //if the distance of moving is over the header height ,
+                    // then show the anim which moves to header position, else show the anim which moves to start position.
                      if(dist >= mHeaderHeight){
                          animToHeader();
                      }else{
@@ -177,7 +213,7 @@ public class TRecyclerView extends FrameLayout{
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
-
+                //when the recycleView has scrolled to the bottom position and the state of RecycleView is IDLE, start to load data.
                 if (newState == RecyclerView.SCROLL_STATE_IDLE) {
                     if (targetInBottom()) {
                       if(mPushRefresh != null){
@@ -185,7 +221,6 @@ public class TRecyclerView extends FrameLayout{
                           mPushRefresh.loadMore();
                       }
                     }
-
                 }
 
             }
@@ -197,7 +232,7 @@ public class TRecyclerView extends FrameLayout{
         });
     }
 
-
+    //create header view
     private void createHeaderView() {
         mHeaderContainer = new RelativeLayout(mCtx);
         mHeaderHeight = (int)mCtx.getResources().getDimension(R.dimen.header_height);
@@ -237,8 +272,6 @@ public class TRecyclerView extends FrameLayout{
         mTAdapter = new TRecyclerAdapter(mCtx, adapter);
         mRecyclerView.setAdapter(mTAdapter);
 
-
-
     }
 
 
@@ -255,14 +288,14 @@ public class TRecyclerView extends FrameLayout{
    }
 
 
-    //回弹到初始位置
+    //the anim which moves to start position
     private void animToStart(){
         ObjectAnimator animator = ObjectAnimator.ofFloat(mRecyclerView,"translationY",  0);
         animator.setDuration(AnimDurConst.ANIM_TO_HEADER_DUR);
         animator.start();
     }
 
-    //回弹到Tips提示的位置
+    //the anim which moves to tip position
     private void animToTip(){
         ObjectAnimator animator = ObjectAnimator.ofFloat(mRecyclerView,"translationY",  mTipHeight);
         animator.setDuration(AnimDurConst.ANIM_TO_TIP_DUR);
@@ -270,7 +303,7 @@ public class TRecyclerView extends FrameLayout{
     }
 
 
-    //回弹到header位置
+    //the anim which moves to header position,w hen the anim is end, start to refresh data
     private void animToHeader(){
         ObjectAnimator animator = ObjectAnimator.ofFloat(mRecyclerView,"translationY",  mHeaderHeight);
         animator.addListener(mToHeaderListener);
@@ -288,6 +321,7 @@ public class TRecyclerView extends FrameLayout{
 
         @Override
         public void onAnimationEnd(Animator animation) {
+            //when the anim of move to header is end, start to refresh data
             if(mPullRefresh != null){
                 mRefresh = true;
                 mPullRefresh.pullRefresh();
@@ -316,10 +350,11 @@ public class TRecyclerView extends FrameLayout{
 
 
     private static class AnimDurConst {
-        //回弹到header的动画时长
+        //the duration of anim to header  position
         private static final  int ANIM_TO_HEADER_DUR = 600;
-        //回弹到初始位置的动画时长
+        //the duration of anim to start position
         private static final  int ANIM_TO_START_DUR = 600;
+        //the duration of anim to tip position
         private static final  int ANIM_TO_TIP_DUR = 300;
     }
 
@@ -332,6 +367,8 @@ public class TRecyclerView extends FrameLayout{
     }
 
 
+    //Set up an observer， when the normal adapter starts to notifyDataChanged，
+    // the real adapter-TRecyclerAdapter starts to notifyDataChanged
     private RecyclerView.AdapterDataObserver mDataObserver = new RecyclerView.AdapterDataObserver() {
         @Override
         public void onChanged() {
