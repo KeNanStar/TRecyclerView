e65717afa0f78ce76c4e28fc6b539a55f7a87ad1
xinghu213839 <xinghu213839@sohu-inc.com>
2018-11-09 12:10:55 +0800

    测试数据修改

--- app/src/main/java/com/robot/recycler/entity/NewsItem.java
+++ app/src/main/java/com/robot/recycler/entity/NewsItem.java
@@ -7,10 +7,10 @@ import com.robot.recycler.TRecyclerUtils;
  * @since 2018/9/18, 下午8:42
  */
 public class NewsItem {
-    public static final String[] TITLES = {"互联网金融", "云计算", "机器学习", "人工智能"};
-    public static final String[] CONTENTS = {"互联网金融不是互联网和金融业的简单结合，而是在实现安全、移动等网络技术水平上，被用户熟悉接受后（尤其是对电子商务的接受），自然而然为适应新的需求而产生的新模式及新业务",
-            "云计算（cloudcomputing）是基于互联网的相关服务的增加、使用和交付模式，通常涉及通过互联网来提供动态易扩展且经常是虚拟化的资源。"
-            , "机器学习(Machine Learning, ML)是一门多领域交叉学科，涉及概率论、统计学、逼近论、凸分析、算法复杂度理论等多门学科。"
+    public static final String[] TITLES = {"欧冠C罗高难凌空斩神球", "任正非最新讲话透露了华为的“野心”和“心机”", "歌手吴亦凡？其实他是个演员", "人工智能:下一次技术革命！！！"};
+    public static final String[] CONTENTS = {"尤文输了，但C罗的表现很出色。虽然主场1-2输给曼联令尤文图斯球迷感到郁闷，但是他们阵中当家前锋C罗却表现很精彩，他攻入了一粒非常漂亮的进球。",
+            "前两天，华为心声论坛公布了任正非十月份在上研所5G业务汇报会上的讲话，其中，任总重点提到，要实现5G网络极简化，把复杂留给自己，简单留给客户——“先从5G SA组网做起，要做到网络架构极简、交易架构极简、网络极安全、隐私保护极可靠、能耗极低，全面实现领先”。"
+            , "不要再纠缠音乐上粉丝刷榜的那点破事，吴亦凡其实是个演员。"
             , "人工智能（Artificial Intelligence），英文缩写为AI。它是研究、开发用于模拟、延伸和扩展人的智能的理论、方法、技术及应用系统的一门新的技术科学。"};
     public static final int[] IMG_IDS = {com.robot.recycler.R.mipmap.pic_1, com.robot.recycler.R.mipmap.pic_2, com.robot.recycler.R.mipmap.pic_3, com.robot.recycler.R.mipmap.pic_4};
 
